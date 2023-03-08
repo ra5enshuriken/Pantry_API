ab9a623654a18933f60a6bbf6a43924ff7793f26
shnn <shnn@softwareag.com>
2023-03-08 16:31:26 +0530

    navigation basic setup done

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,19 +2,26 @@ import { LoginComponent } from "./components/login/login.component";
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 const routes: Routes = [
+ 
   {
     path:"",
-    component:AppComponent
+    redirectTo:"login",
+    pathMatch:"full"
   },
   {
     path:"login",
     component:LoginComponent
   },
   {
+    path:"nav",
+    component:NavbarComponent
+  },
+  {
     path:"**",
-    component:LoginComponent
+    component:AppComponent
   }
 ];
 
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  
   menuToggle=true;
+  mobile=false;
+  
+  ngOnInit(): void {
+    this.mobile=(window.innerWidth<750);
+  }
+  
+  @HostListener('window:resize', ['$event']) 
+  onResize(){
+      this.mobile=(window.innerWidth<750);
+      
+  }
   toggle(e:any){
     this.menuToggle=!this.menuToggle;
     console.log("toggled");
